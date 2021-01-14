487dcb360af56d5499fed10279e6407cb3dead7d
Jeremy Dombrowski <jeremy@meatflavour.dev>
2021-01-13 19:55:03 -0800

    Exercise 7 and validation

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -6,7 +6,13 @@ require_relative './exercise_4'
 require_relative './exercise_5'
 require_relative './exercise_6'
 
-puts "Exercise 7"
-puts "----------"
+puts 'Exercise 7'
+puts '----------'
 
-# Your code goes here ...
+print 'Store name: '
+store_name = gets.chomp
+create_store = Store.create(name: store_name)
+
+create_store.errors.full_messages.each do |error|
+  puts error
+end
--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,7 @@
 class Employee < ActiveRecord::Base
   belongs_to :store
+  validates_presence_of :first_name
+  validates_presence_of :last_name
+  validates_numericality_of :hourly_rate, greater_than_or_equal_to: 40, less_than_or_equal_to: 200
+  validates_presence_of :store_id
 end
